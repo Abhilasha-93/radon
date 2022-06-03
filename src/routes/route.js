fededc4fe3d49796f9805ecf9066d4dc78590eed
Abhilasha-93 <aabhilasha.verma@gmail.com>
2022-06-04 01:22:14 +0530

    completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,95 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+//Write a POST /players api that saves a player’s details and doesn’t allow saving the data of a player with a name that already exists in the data
 
+let players = [
+    {
+        "name": "Manish",
+        "dob": "1/9/1995",
+        "gender": "male",
+        "city": "jalandar",
+        "sports": ["swimming"
+        ],
+    },
+    {
+        "name": "Gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": ["soccer"
+        ],
+    },
+    {
+        "name": "Lokesh",
+        "dob": "1/5/1995",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": ["cricket"
+        ],
+    },
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+]
 
+router.post('/players', function (req, res) {
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+    if (isNameRepeated) {
+        res.send("this player is already exist")
+    }
+    else {
+        players.push(newPlayer)
+        res.send(players)
+    }
 })
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
+
+
+
+
+
+
+
+
+
+router.get('/sol1', function (req, res) {
+    let arr = [1, 2, 3, 5, 6, 7]
+    let item = arr[0]
+    let n = 0
+    for (let i = 0; i < arr.length + 1; i++) {
+        if (arr[i] == item) {
+            item++
+        } else {
+            missingNumber = item
+        }
+    }
+    const sumOfTheArray = (n * (n + 1)) / 2 - missingNumber
+    res.send({ missingNumber });
+});
+router.get('/sol2', function (req, res) {
+    let arr = [33, 34, 35, 37, 38]
+    let item = arr[0]
+    let n = 0
+    for (let i = 0; i < arr.length + 1; i++) {
+        if (arr[i] == item) {
+            item++
+        } else {
+            missingNumber = item
+        }
+    }
+    const sumOfTheArray = (n * (n + 1)) / 2 - missingNumber
+    res.send({ missingNumber });
+});
 module.exports = router;
